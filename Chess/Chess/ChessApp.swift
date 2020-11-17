d892b94351a18581a214ed3b9b72e3bfa75713fd
Golden Thumb <zhijunsheng@mbp2020.local>
2020-11-17 07:43:45 -0500

    created SwiftUI project for chess macOS

---
+++ Chess/Chess/ChessApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessApp.swift
+//  Chess
+//
+//  Created by Golden Thumb on 2020-11-17.
+//
+
+import SwiftUI
+
+@main
+struct ChessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chess/Chess/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Chess
+//
+//  Created by Golden Thumb on 2020-11-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
